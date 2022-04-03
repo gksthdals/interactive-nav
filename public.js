e2e85bbde3df753dde61644c0ffa171a4c8e0cc3
gksthdals <tiranosm@naver.com>
2022-04-03 22:36:25 +0900

    track currentLocation

--- public.js
+++ public.js
@@ -2,41 +2,21 @@ import React, { useState, useEffect } from "react";
 import {
   ActivityIndicator,
   StyleSheet,
+  Alert,
   Text,
   View,
   ScrollView,
   Dimensions,
-  TouchableOpacity,
 } from "react-native";
-import { WebView } from "react-native-webview";
-import MapView, {
-  Marker,
-  AnimatedRegion,
-  MarkerAnimated,
-} from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import JSSoup from "jssoup";
 import * as TaskManager from "expo-task-manager";
 import * as Location from "expo-location";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const LOCATION_TASK_NAME = "LocationUpdate";
-const JAVASCRIPT_API_KEY = "API_KEY";
-
-// Location Variables
-let currentLocation = null;
-let prevBusLocation = null;
-TaskManager.defineTask(LOCATION_TASK_NAME, ({ data: { locations }, error }) => {
-  if (error) {
-    // check `error.message` for more details.
-    return;
-  }
-  const currentLoc = {
-    latitude: locations[0]["coords"]["latitude"],
-    longitude: -locations[0]["coords"]["longitude"],
-  };
-  console.log(currentLoc);
-  currentLocation = currentLoc;
-});
+const REST_API_KEY = "2c5496592ddc68a5a57199f34a037b7a";
+const CLOSED_DISTANCE = 1e-8;
 
 async function requestPermissions() {
   const foregroundPromise = await Location.requestForegroundPermissionsAsync();
@@ -53,14 +33,40 @@ async function requestPermissions() {
 }
 
 export default function Public(props) {
+  TaskManager.defineTask(
+    LOCATION_TASK_NAME,
+    ({ data: { locations }, error }) => {
+      if (error) {
+        // check `error.message` for more details.
+        return;
+      }
+      const currentLoc = {
+        latitude: parseFloat(locations[0]["coords"]["latitude"]) + 0.26,
+        latitudeDelta: 0.010011167287203193,
+        longitude: parseFloat(-locations[0]["coords"]["longitude"]) + 5,
+        longitudeDelta: 0.008252863819677714,
+      };
+      // console.log("currentLoc:", currentLoc);
+      // setLocation(currentLoc);
+    }
+  );
   // routes.keys: { "data-sx", "data-sy", "data-ex", "data-ey", "class", "txt_station",
   //                "bus_num", "txt_detail", "data-id", "data-buses", "subway_num" }
   const [routes, setRoutes] = useState(null);
+  // current route index
+  const [routeIndex, setRouteIndex] = useState(1);
+  // current location
   const [location, setLocation] = useState({
     // Korea Univ.
     latitude: 37.58520547371376,
+    latitudeDelta: 0.010000000000594866,
     longitude: 127.02547413870877,
+    longitudeDelta: 0.008243657890659506,
   });
+  const [nextLocation, setNextLocation] = useState(null);
+  const [toNext, setToNext] = useState(true);
+  const [prevBusLoc, setPrevBusLoc] = useState(null);
+  const [toPrevBusLoc, setToPrevBusLoc] = useState(true);
 
   const getListSectionListDetail = (htmlText) => {
     const soup = new JSSoup(htmlText);
@@ -153,10 +159,13 @@ export default function Public(props) {
     const articleElement = justBeforeBusStopSoup.find("article");
     const busStopLocation =
       articleElement.contents[3].contents[1].contents[1].contents[1];
-    changeCoords({
-      "data-wx": busStopLocation.attrs["data-wx"],
-      "data-wy": busStopLocation.attrs["data-wy"],
-    });
+    changeCoords(
+      {
+        "data-wx": busStopLocation.attrs["data-wx"],
+        "data-wy": busStopLocation.attrs["data-wy"],
+      },
+      "prev"
+    );
   };
   const getJustBeforeBusStopHTML = (href) => {
     const justBeforeBusStopUrl = `https://m.map.kakao.com${href}`;
@@ -212,19 +221,28 @@ export default function Public(props) {
     };
     request.send();
   };
-  const changeCoords = (WCONGNAMUL) => {
+  const changeCoords = (WCONGNAMUL, type) => {
     const url = `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${WCONGNAMUL["data-wx"]}&y=${WCONGNAMUL["data-wy"]}&input_coord=WCONGNAMUL&output_coord=WGS84`;
     const request = new XMLHttpRequest();
     request.open("GET", url, true);
-    request.setRequestHeader("Authorization", "KakaoAK " + JAVASCRIPT_API_KEY);
+    request.setRequestHeader("Authorization", "KakaoAK " + REST_API_KEY);
     request.onload = () => {
       const document = JSON.parse(request.responseText).documents[0];
-      prevBusLocation = { latitude: document["y"], longitude: document["x"] };
-      console.log(prevBusLocation);
+      const WGS84 = { latitude: document["y"], longitude: document["x"] };
+      if (type === "next") {
+        console.log(routeIndex + "번째 Route\nnextLocation :", WGS84);
+        setNextLocation(WGS84);
+      } else if (type === "prev") {
+        console.log("prevBusStop :", WGS84);
+        setPrevBusLoc(WGS84);
+      }
     };
     request.send();
   };
   useEffect(() => {
+    // request fore/background permissions
+    requestPermissions();
+    // request public routes & details
     const request = new XMLHttpRequest();
     request.open("GET", props.url, true);
     request.onload = () => {
@@ -232,6 +250,65 @@ export default function Public(props) {
     };
     request.send();
   }, []);
+  useEffect(() => {
+    if (routes === null) return;
+
+    if (nextLocation === null) {
+      changeCoords(
+        {
+          "data-wx": routes[routeIndex]["data-ex"],
+          "data-wy": routes[routeIndex]["data-ey"],
+        },
+        "next"
+      );
+    }
+    // if current route is public_bus depart -> get bus route and set prev bus stop location
+    if (routes[routeIndex]["class"] === "public_bus depart") {
+      const reply = Alert.alert(
+        "Public bus depart",
+        "Do you want to receive Alert at previous bus stop?"
+      );
+      getBusInfoHTML([routes[routeIndex], routes[routeIndex + 1]]);
+    } else {
+      setPrevBusLoc(null);
+    }
+  }, [routes, routeIndex]);
+  useEffect(() => {
+    if (nextLocation === null) return;
+
+    // about routeIndex
+    const distToNext =
+      (location.latitude - nextLocation.latitude) ** 2 +
+      (location.longitude - nextLocation.longitude) ** 2;
+
+    if (toNext === true && distToNext < CLOSED_DISTANCE) {
+      setToNext(false);
+    } else if (toNext === false && distToNext > CLOSED_DISTANCE) {
+      setRouteIndex(routeIndex+1);
+      setToNext(true);
+    }
+
+    // alert at previous bus stop
+    if (prevBusLoc === null) return;
+
+    const distToPrev =
+      (location.latitude - prevBusLoc.latitude) ** 2 +
+      (location.longitude - prevBusLoc.longitude) ** 2;
+    if (
+      prevBusLoc !== null &&
+      toPrevBusLoc === true &&
+      distToPrev < CLOSED_DISTANCE
+    ) {
+      setToPrevBusLoc(false);
+    } else if (
+      prevBusLoc !== null &&
+      toPrevBusLoc === false &&
+      distToPrev > CLOSED_DISTANCE
+    ) {
+      Alert.alert("Getting off next bus stop!");
+      setToPrevBusLoc(true);
+    }
+  }, [location]);
   return (
     <View style={{ flex: 1 }}>
       <View
@@ -241,63 +318,54 @@ export default function Public(props) {
       >
         <MapView
           style={{ flex: 1 }}
-          initialRegion={{
-            latitude: 37.58520547371376,
-            longitude: 127.02547413870877,
-            latitudeDelta: 0.00922,
-            longitudeDelta: 0.00421,
+          initialRegion={location}
+          onRegionChange={(region) => {
+            setLocation(region);
           }}
-          onRegionChange={() => {
-            setLocation(currentLocation);
-          }}
-          showsMyLocationButton
-          showsUserLocation
-        />
+          // region={location}
+        >
+          <Marker
+            coordinate={{
+              latitude: location["latitude"],
+              longitude: location["longitude"],
+            }}
+          />
+        </MapView>
       </View>
       <View style={{ flex: 1, backgroundColor: "skyblue" }}>
-        <ScrollView
-          pagingEnabled
-          horizontal
-          contentContainerStyle={styles.routes}
-        >
-          {routes === null ? (
-            <View style={{ justifyContent: "center", alignItems: "center" }}>
-              <ActivityIndicator />
-            </View>
-          ) : (
-            routes.map((route, index) => (
+        {routes === null ? (
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <ActivityIndicator />
+          </View>
+        ) : (
+          <ScrollView
+            pagingEnabled
+            horizontal
+            contentContainerStyle={styles.routes}
+            contentOffset={{ x: SCREEN_WIDTH * routeIndex, y: 0 }}
+          >
+            {routes.map((route, index) => (
               <View key={index} style={styles.route}>
-                <View style={styles.route_details}>
+                <View
+                  style={
+                    index === routeIndex
+                      ? { ...styles.route_details, backgroundColor: "yellow" }
+                      : { ...styles.route_details, backgroundColor: "grey" }
+                  }
+                >
                   <Text>{route["class"]}</Text>
                   <Text>{route["txt_station"]}</Text>
                   {route["class"] === "public_bus depart" ? (
                     <View>
                       <Text>{route["bus_num"]}</Text>
                       <Text>{route["txt_detail"]}</Text>
-                      <TouchableOpacity
-                        onPress={() => {
-                          getBusInfoHTML([routes[index], routes[index + 1]]);
-                        }}
-                      >
-                        <Text>prevBusLocation</Text>
-                      </TouchableOpacity>
-                      <Text>
-                        {prevBusLocation === null
-                          ? ""
-                          : prevBusLocation["latitude"]}
-                      </Text>
-                      <Text>
-                        {prevBusLocation === null
-                          ? ""
-                          : prevBusLocation["longitude"]}
-                      </Text>
                     </View>
                   ) : null}
                 </View>
               </View>
-            ))
-          )}
-        </ScrollView>
+            ))}
+          </ScrollView>
+        )}
       </View>
     </View>
   );
@@ -313,7 +381,6 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
   },
   route_details: {
-    backgroundColor: "grey",
     width: SCREEN_WIDTH * 0.9,
     height: "80%",
     marginVertical: "5%",
